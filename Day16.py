dfa81da279625743ee6eadc0ff80aa6fba63f185
Yatharth Jangid <yatharth77770@gmail.com>
2022-10-10 14:28:14 +0530

    changes

---
+++ Day16.py
@@ -0,0 +1,17 @@
+import tkinter as tkt
+app=tkt.Tk()
+
+app.title("my app")
+app.geometry("600x400")
+
+tkt.Label(app,text='a single text label').place(x=50,y=50)
+
+def abc():
+    print('wow')
+
+tkt.Label(app, text="Yatharth Jangid").place(x=100,y=100)
+
+tkt.Button(app, text="isko click kar", command=abc).place(x=10,y=10)
+tkt.Button(app, text="yeh wala bhi", command=lambda:print('wow')).place(x=30,y=30)
+
+app.mainloop()
